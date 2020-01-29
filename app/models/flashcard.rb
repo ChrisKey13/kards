bed8da79a1d6605e4672edbecb83520eac820b80
christophe bouquet <christophe.productowner@gmail.com>
2020-01-29 01:50:03 +0100

    Active Storage Implemented

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,10 +1,10 @@
 class Flashcard < ApplicationRecord
   belongs_to :list
-  validates :title, presence: :true, uniqueness: :true
+  validates :title, presence: :true
   validates :content, presence: :true
   validates :solution, presence: :true
   validates :answer, absence: :true
-  has_many_attached :photos
+  has_one_attached :photo
 
 
 end
