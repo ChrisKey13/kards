3b25c02c4649c9a965f51cd1d075eb26c96b6f05
christophe bouquet <christophe.productowner@gmail.com>
2020-03-04 20:52:20 +0100

    dashboard advancing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include MarkdownHelper
+   protect_from_forgery prepend: true
 
+
+
+   def after_sign_in_path_for(resource)
+     stored_location_for(resource) || lists_path
+   end
 end
